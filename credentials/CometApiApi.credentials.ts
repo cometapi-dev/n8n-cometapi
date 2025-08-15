e88df4260a183844e1d7da46c3537df160230044
alanyear <917022897@qq.com>
2025-08-15 14:13:34 +0800

    0.2.8

--- credentials/CometApiApi.credentials.ts
+++ credentials/CometApiApi.credentials.ts
@@ -1,9 +1,9 @@
-﻿import { ICredentialType, INodeProperties } from 'n8n-workflow';
+﻿import { ICredentialType, INodeProperties ,ICredentialTestRequest} from 'n8n-workflow';
 
 export class CometApiApi implements ICredentialType {
     name = 'cometApiApi';
     displayName = 'CometAPI API';
-    documentationUrl = 'https://cometapi.example.com/docs';
+    documentationUrl = 'https://api.cometapi.com/docs';
     properties: INodeProperties[] = [
         {
             displayName: 'API Key',
@@ -11,11 +11,17 @@ export class CometApiApi implements ICredentialType {
             type: 'string',
             default: '',
             required: true,
-            typeOptions: { 
-                password: true 
+            typeOptions: {
+                password: true
             },
             placeholder: 'your-api-key-here',
             description: 'Your CometAPI API Key.',
         },
     ];
+			test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.cometapi.com.',
+			url: '',
+		},
+	};
 }
